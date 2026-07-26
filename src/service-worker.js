4c31f013b3d6c2887abb6ebd836f995b58fb7b18
Ryan Berliner <ryan@ryanberliner.com>
2026-07-26 09:38:13 -0400

    fix service worker registration in prod

--- src/service-worker.js
+++ src/service-worker.js
@@ -3,7 +3,7 @@
 // This version number and filelist is generated automatically from the build
 // script in scripts/build.sh.
 //
-const AUTOGEN_CACHE_VERSION = '09ebd8b47bed870d4e5a477e45b5a5e82d675a72';
+const AUTOGEN_CACHE_VERSION = '777aecfff8147a11d17c453fff8ebfddd9e6960f';
 const AUTOGEN_CACHE_ASSETS = ['index.html','app/log-hours.js','app/log-hours.css','app/dialog.js','app/index.js','app/index.css','app/data.js','app/dialog.css','app/timeline/render-day.js','lib/timeline.js','lib/node.js','lib/state.js','lib/date.js','button.css','timeline/hour-block.css','timeline/day-header.css','timeline/day-header.js','timeline/day.css','timeline/day-timeline.js','timeline/day.js','timeline/hour-block.js','base.css'];
 
 // This is deployed a my github pages site, which contains other projects
